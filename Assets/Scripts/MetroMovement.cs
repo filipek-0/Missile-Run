79acb407b94e0dce3999c816d9a40fc7f8f3ca7a
FiHre <fihrehovcik@gmail.com>
2021-03-08 09:25:10 +0100

    Level 1,2,3,4, Unity colors

---
+++ Assets/Scripts/MetroMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MetroMovement : MonoBehaviour
+{
+    public float movingSpeed;
+    public bool startMovement = false;
+    public bool destroyMetro = false;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (startMovement == true)
+        {
+            transform.Translate(Vector3.up * movingSpeed * Time.deltaTime);
+        }
+        if (destroyMetro == true)
+        {
+            GameObject.Destroy(gameObject);
+            return;
+        }
+    }
+    public void StartMovement()
+    {
+        startMovement = true;
+    }
+
+    public void StopMovement()
+    {
+        destroyMetro = true;
+    }
+}
---
+++ Assets/Scripts/TriggerMetroStart.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerMetroStart : MonoBehaviour
+{
+    public MetroMovement metroMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        metroMovement.GetComponent<MetroMovement>().StartMovement();
+    }
+}
