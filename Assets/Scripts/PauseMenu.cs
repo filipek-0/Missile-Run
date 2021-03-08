55c8aeb77044388896e9024f9f9b57f0bf6dca91
FiHre <fihrehovcik@gmail.com>
2021-03-08 21:04:16 +0100

    Main Menu, Credits

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused = false;
+    [SerializeField] GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused == false)
+            {
+                PauseGame();
+            }
+            else
+            {
+                ResumeGame();
+            }
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+        gameIsPaused = true;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+        gameIsPaused = false;
+    }
+    public void LoadMenuFromPause()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1f;
+    }
+    public void QuitGamefromPause()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
