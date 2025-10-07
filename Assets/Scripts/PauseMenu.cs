75f2c4357a38ac356d3ce15debeb8169315b417a
filipek <filip.hrehovcik.yt@gmail.com>
2025-10-07 20:04:19 +0100

    wrapping up together

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -8,6 +8,8 @@ public class PauseMenu : MonoBehaviour
     public static bool gameIsPaused;
     [SerializeField] GameObject pauseMenu;
     [SerializeField] GameObject ControlsMenu;
+    [SerializeField] AudioSource audioSource;
+    [SerializeField] float delay = 0.3f;
 
     private void Start()
     {
@@ -34,25 +36,43 @@ public class PauseMenu : MonoBehaviour
 
     void PauseGame()
     {
+        audioSource.Play();
         Time.timeScale = 0f;
         pauseMenu.SetActive(true);
         gameIsPaused = true;
     }
     public void ResumeGame()
     {
+        audioSource.Play();
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
         gameIsPaused = false;
     }
-    public void LoadMenuFromPause()
+    public void MenuButton()
+    {
+        audioSource.Play();
+        Time.timeScale = 1f;
+        Invoke("LoadMenuFromPause", delay);
+    }
+    private void LoadMenuFromPause()
     {
         SceneManager.LoadScene(0);
         pauseMenu.SetActive(false);
         Time.timeScale = 1f;
     }
-    public void QuitGamefromPause()
+    public void QuitButton()
+    {
+        audioSource.Play();
+        Time.timeScale = 1f;
+        Invoke("QuitGameFromPause", delay);
+    }
+    private void QuitGameFromPause()
     {
         Application.Quit();
         Debug.Log("Quit");
+    }
+    public void ControlsButton()
+    {
+        audioSource.Play();
     }
 }
